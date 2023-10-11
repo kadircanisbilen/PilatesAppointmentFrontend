91fa461c05d9491dab3e989e67eb84af047f26e3
Kadircan ISBILEN <kadircanisbilen@gmail.com>
2023-10-11 03:47:10 +0300

    first commit

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,17 @@
+export function SignUp(){
+    return <>
+    <h1> Sign Up </h1>
+    <div>
+    <label htmlFor="name">Name</label>
+    <input id="name"/>
+    </div>
+    <div>
+    <label htmlFor="surname">Surname</label>
+    <input id="surname"/>
+    </div>
+    <div>
+    <label htmlFor="email">Email</label>
+    <input id="email"/>
+    </div>
+    </> 
+}
