b45fcb8ddebf1b0c9e52d0c0accf66381ae5d965
Kadircan ISBILEN <kadircanisbilen@gmail.com>
2023-10-13 04:08:28 +0300

    Sending request to backend, some cosmetics, error and success messages...

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,116 @@
-export function SignUp(){
-    return <>
-    <h1> Sign Up </h1>
-    <div>
-    <label htmlFor="name">Name</label>
-    <input id="name"/>
+import axios from "axios";
+import { useState } from "react";
+import { signUp } from "./api";
+
+export function SignUp() {
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+  const [apiProgress, setApiProgress] = useState(false);
+  const [successMessage, setSuccessMessage] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setSuccessMessage();
+    setApiProgress(true);
+    try {
+      const response = await signUp({
+        name,
+        surname,
+        email,
+        password,
+      });
+      setSuccessMessage(response.data.message);
+    } catch {
+    } finally {
+      setApiProgress(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+        <form className="card" onSubmit={onSubmit}>
+          <div className="card-header text-center">
+            <h1> Sign Up </h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                id="name"
+                className="form-control"
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="surname" className="form-label">
+                Surname
+              </label>
+              <input
+                id="surname"
+                className="form-control"
+                onChange={(event) => setSurname(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                id="email"
+                className="form-control"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordRepeat" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                id="passwordRepeat"
+                type="password"
+                className="form-control"
+                onChange={(event) => setPasswordRepeat(event.target.value)}
+              />
+            </div>
+            {successMessage && (
+              <div className="alert alert-success"> {successMessage}</div>
+            )}
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={
+                  apiProgress || !password || password !== passwordRepeat
+                }
+              >
+                {apiProgress && (
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                  ></span>
+                )}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-    <div>
-    <label htmlFor="surname">Surname</label>
-    <input id="surname"/>
-    </div>
-    <div>
-    <label htmlFor="email">Email</label>
-    <input id="email"/>
-    </div>
-    </> 
+  );
 }
