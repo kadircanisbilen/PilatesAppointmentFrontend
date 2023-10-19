ad9daf3dde9bf0ed34fb87f447b7c11c91b445dd
Kadircan ISBILEN <kadircanisbilen@gmail.com>
2023-10-19 04:12:56 +0300

    19.10.2023 04:12

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { signUp } from "./api";
+import { Input } from "./components/Input";
 
 export function SignUp() {
   const [name, setName] = useState();
@@ -10,10 +11,58 @@ export function SignUp() {
   const [passwordRepeat, setPasswordRepeat] = useState();
   const [apiProgress, setApiProgress] = useState(false);
   const [successMessage, setSuccessMessage] = useState();
+  const [errors, setErrors] = useState({});
+  const [generalError, setGeneralError] = useState();
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        email: undefined,
+      };
+    });
+  }, [email]); //If email changes, set errors to blank.
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        name: undefined,
+      };
+    });
+  }, [name]); //If name changes, set errors to blank.
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        surname: undefined,
+      };
+    });
+  }, [surname]); //If surname changes, set errors to blank.
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        password: undefined,
+      };
+    });
+  }, [password]); //If password changes, set errors to blank.
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        passwordRepeat: undefined,
+      };
+    });
+  }, [passwordRepeat]); //If passwordRepeat changes, set errors to blank.
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setSuccessMessage();
+    setGeneralError();
     setApiProgress(true);
     try {
       const response = await signUp({
@@ -23,11 +72,25 @@ export function SignUp() {
         password,
       });
       setSuccessMessage(response.data.message);
-    } catch {
+    } catch (axiosError) {
+      if (
+        axiosError.response?.data &&
+        axiosError.response.data.status === 400
+      ) {
+        setErrors(axiosError.response.data.validationErrors);
+      } else {
+        setGeneralError("Unexpected error. Please try again.");
+      }
     } finally {
       setApiProgress(false);
     }
   };
+
+  const passwordMatchError = useMemo(() => {
+    if (password && password !== passwordRepeat) {
+      return "Password fields must match.";
+    }
+  }, [password, passwordRepeat]); // Only if password or passwordRepeat fields changes, then check. Don't check every time. (For performance)
 
   return (
     <div className="container">
@@ -37,60 +100,43 @@ export function SignUp() {
             <h1> Sign Up </h1>
           </div>
           <div className="card-body">
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                Name
-              </label>
-              <input
-                id="name"
-                className="form-control"
-                onChange={(event) => setName(event.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="surname" className="form-label">
-                Surname
-              </label>
-              <input
-                id="surname"
-                className="form-control"
-                onChange={(event) => setSurname(event.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                id="email"
-                className="form-control"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="form-control"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="passwordRepeat" className="form-label">
-                Password Repeat
-              </label>
-              <input
-                id="passwordRepeat"
-                type="password"
-                className="form-control"
-                onChange={(event) => setPasswordRepeat(event.target.value)}
-              />
-            </div>
+            <Input
+              id="name"
+              label="Name"
+              error={errors.name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <Input
+              id="surname"
+              label="Surname"
+              error={errors.surname}
+              onChange={(event) => setSurname(event.target.value)}
+            />
+            <Input
+              id="email"
+              label="Email"
+              error={errors.email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <Input
+              id="password"
+              label="Password"
+              error={errors.password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+            />
+            <Input
+              id="passwordRepeat"
+              label="Password Repeat"
+              error={passwordMatchError}
+              onChange={(event) => setPasswordRepeat(event.target.value)}
+              type="password"
+            />
             {successMessage && (
               <div className="alert alert-success"> {successMessage}</div>
+            )}
+            {generalError && (
+              <div className="alert alert-danger"> {generalError}</div>
             )}
             <div className="text-center">
               <button
