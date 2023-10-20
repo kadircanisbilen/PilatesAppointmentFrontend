03e2e4d806a32d9073dac5e070a6d20ec71f7875
Kadircan ISBILEN <kadircanisbilen@gmail.com>
2023-10-20 05:36:55 +0300

    Language support

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 //import App from './App.jsx'
 //import './index.css'
-import { SignUp } from './pages/SignUp/index.jsx'
-import './styles.scss'
+import { SignUp } from "./pages/SignUp/index.jsx";
+import "./styles.scss";
+import "./locales";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SignUp/>
-  </React.StrictMode>,
-)
+    <SignUp />
+  </React.StrictMode>
+);
